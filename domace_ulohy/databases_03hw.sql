8532402d383f360113b68ebb7167a410f4698af2
František Trebuňa <ferotre@gmail.com>
2020-05-07 11:08:48 +0200

    Finish hw3

--- domace_ulohy/databases_03hw.sql
+++ domace_ulohy/databases_03hw.sql
@@ -47,3 +47,86 @@ inner join CourseStudent
 	on CourseStudent.StudentID = Student.ID and CourseStudent.Grade is not null
 inner join Course 
 	on CourseStudent.CourseID = Course.ID and Course.Name = 'Video retrieval'
+
+
+-- študenti, ktorí majú ako vedúceho diplomovky niekoho s menom Jane
+-- vypíš ich ID a Meno
+select Student.ID, Student.Name 
+from Student 
+inner join Teacher
+	on Teacher.ID = Student.DT_SupervisorID
+where 
+	Teacher.Name = 'Jane'
+
+
+-- uèitelia, ktorí sú vedúci aj diplomky aj bakalárky nejakého študenta
+select distinct Teacher.*
+from Teacher 
+inner join Student 
+	on Student.BT_SupervisorID = Teacher.ID and Student.DT_SupervisorID = Teacher.ID
+
+-- uèitelia s rovnakým menom ako študent, ktorého vedú
+select Teacher.ID, Student.ID, Teacher.Name
+from Teacher
+inner join Student
+	on Student.BT_SupervisorID = Teacher.ID or Student.DT_SupervisorID = Teacher.ID
+where
+	Teacher.Name = Student.Name
+
+-- kurzy vypísané v poradí pod¾a poètu študentov, ktorí sú na nich zapísaní
+select Course.Name, T2.Students
+from Course 
+
+inner join (
+	select CourseID, count(StudentID) as Students
+	from CourseStudent
+	group by CourseID) as T2
+	
+	on T2.CourseID = Course.ID
+
+order by T2.Students
+
+-- kurzy striedené pod¾a poètu študentov, ktorí ešte nedostali žiadnu známku
+select Course.Name, T2.Students
+from Course
+
+inner join(
+	select CourseID, count(StudentID) as Students
+	from CourseStudent
+	where Grade is null
+	group by CourseID) as T2
+
+	on T2.CourseID = Course.ID
+
+order by T2.Students
+
+-- študenti, ktorí nenavštevujú žiadne kurzy
+select Student.*
+from Student
+left outer join CourseStudent
+	on CourseStudent.StudentID = Student.ID
+where 
+	CourseStudent.StudentID is null
+
+-- študenti s najvyšším poètom navštevovaných kurzov
+select top(1) Student.ID, Student.Name, T2.sCount
+from Student
+inner join (
+		select StudentID, count(CourseID) as sCount
+		from CourseStudent
+		group by StudentID ) as T2
+	on T2.StudentID = Student.ID
+
+order by T2.sCount DESC
+
+-- študenti, ktorí navštevujú Tomove kurzy
+select Student.ID, Student.Name 
+from Student
+inner join CourseStudent 
+	on CourseStudent.StudentID = Student.ID
+inner join Course 
+	on Course.ID = CourseStudent.CourseID
+inner join Teacher 
+	on Teacher.ID = Course.TeacherID
+
+where Teacher.Name = 'Tom'
